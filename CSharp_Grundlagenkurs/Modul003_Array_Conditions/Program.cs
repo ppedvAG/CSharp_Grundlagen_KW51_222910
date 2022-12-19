c1cdd039fbe601c6fdf2ca192e005fa84d7975b9
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-12-19 14:11:47 +0100

    Tag 1

---
+++ CSharp_Grundlagenkurs/Modul003_Array_Conditions/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        //ARRAYS
+        ///Arrays sind Collections, welche mehrere Variablen eines Datentyps speichern können. Die Größe des Arrays muss bei der
+        ///Initialisierung entweder durch eine Zahl oder durch eine bestimmte Anzahl von spezifischen Elementen definiert werden.
+        int[] zahlen = { 2, 4, 78, -123, -8, 0, 11111 }; //shortcut
+
+        int[] zahlen1 = new int[] { 2, 4, 3, 34, 54 }; 
+
+        //Der Zugiff auf einzelne Array-Positionen erfolgt durch einen Nullbasierten Index
+        Console.WriteLine(zahlen[2]); //78
+        zahlen[2] = 1234;
+        Console.WriteLine(zahlen[2]); //1234
+
+        //Array-Deklaration ohne direkte Initialisierung der Positionen (Größe muss angegeben werden)
+        string[] worte = new string[5];
+
+
+        //Achtung das sind keine Array-Methoden
+        //Viele der Array-Methoden werden via -> using System.Linq angeboten 
+        //u.a Count(), First(), FirstOrDefault(), Min(), Max(), Sum(), Average(), Contains(), 
+
+
+        //Woichtig ist, wenn Linq-Erweiterungsmethoden angeboten werden, dann: 
+        //zahlen.Length ist schneller als zahlen.Count()
+
+
+        Console.WriteLine(zahlen.Length);//Hier bekommt man sofort die Array-Größe ausgegeben
+        Console.WriteLine(zahlen.Count());//Hier wird ermittelt 
+
+
+
+        #region Zweidimensionales Array
+
+        //Mehrdimensionales Array
+        int[,] zweiDimArray = new int[3, 5];
+        zweiDimArray[0, 0] = 1;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                zweiDimArray[i, j] = i * j;
+            }
+        }
+        Console.WriteLine(zweiDimArray[2, 3]);
+        #endregion
+
+
+        #region If-Statement
+
+
+        //Deklaration und Initialisierung von Beispiel-Variablen
+        int a = 23;
+        int b = 23;
+
+        //IF-ELSEIF-ELSE-Block
+        ///Das Programm wird den ersten Block ausführen, bei welchem er auf eine wahre Bedingung trifft und dann am Ende des Blocks mit
+        ///dem Code weiter machen
+        if (a < b)
+        {
+            Console.WriteLine("A ist kleiner als B");
+        }
+        //Es kann beliebig viele ELSE-IF-Blöcke geben
+        else if (a > b)
+        {
+            Console.WriteLine("A ist größer als B");
+        }
+        //Wenn keine der Bedingungen wahr ist, wird der (optionale) ELSE-Block ausgeführt
+        else
+            Console.WriteLine("A ist gleich B");
+        #endregion
+
+
+        #region Kurz-Notation
+
+        //Syntax: Bedinung ? TrueAusgabe : FalseAusgabe
+        string ergebnis = (a == b) ? "A ist gleich B" : "A ist ungleich B";
+        #endregion
+
+
+        #region String im Vergleich
+
+        //Bsp eines String-Vergleichs
+        string name1 = "Hans";
+        string name2 = "Hans";
+
+
+        //Equals = gleich 
+        if (name1.Equals(name2))
+        {
+            Console.WriteLine("Gleich");
+        }
+
+        if (name1 == name2)
+            Console.WriteLine("gleich");
+
+        #endregion  
+
+
+        DateTime.IsLeapYear(2000);
+
+    }
+}
---
+++ CSharp_Grundlagenkurs/Modul004Demo/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace Modul004Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Schleifen
+
+            int a = 0;
+            int b = 10;
+
+            //WHILE-Schleife
+            ///Die WHILE-Schleife wird wiederholt, solange die Bedingung wahr ist. Ist die Bedingung von vornherein unwahr, dann wird die Schleife
+            ///übersprungen
+            while (a < b)
+            {
+                Console.WriteLine("A ist kleiner B");
+                a++;
+
+                //Vorab können wir eine Schleife abbrechen
+
+                if (a == 5)
+                    break; 
+
+            }
+            Console.WriteLine("Schleifenende");
+
+
+            a = 0;
+
+            do
+            {
+                
+                a++;
+
+                if (a % 2 != 0)
+                    continue;
+
+                Console.WriteLine(a);
+
+               
+            } while (a < 10);
+
+
+
+
+
+
+
+
+            //FOR-Schleife
+            ///Der FOR-Schleife wird ein Laufindex (i) sowie eine Bedingung und eine Anweisung übergeben. Am Ende jedes Durchlaufes wird die
+            ///Anweisung ausgeführt. Wenn die Bedingung nicht (mehr) wahr ist, wird kein (weiterer) Schleifendurchlauf begonnen.
+            for (int i = 0; i < 10; i += 2)
+            {
+                Console.WriteLine(i);
+
+                i++;
+            }
+
+
+
+
+            int[] zahlen = { 2, 3, 5, 4 };
+            //Iteration über ein Array mittels For-Schleife
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.WriteLine(zahlen[i]);
+            }
+
+            //FOREACH-Schleifen können über Collections laufen und sprechen dabei jedes Element genau einmal an
+            foreach (int item in zahlen)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+            #endregion
+
+            #region Endlosschleife
+            //while (true) //Endlosschleife
+            //{
+
+            //    //Darstellung eines Hauptmenüs 
+            //    ConsoleKeyInfo key =  Console.ReadKey();
+
+                
+            //    if (key.Key == ConsoleKey.Q)
+            //        break;
+            //}
+
+            ////Würde gehen...aber man nimmt eine for-Schleife
+            //for (;true;)
+            //{
+            //    ConsoleKeyInfo key = Console.ReadKey();
+
+
+            //    if (key.Key == ConsoleKey.Q)
+            //        break;
+            //}
+            #endregion
+
+
+            #region Enums
+            //ENUMERATOREN sind spezialisierte selbst-definierte Datentypen mit festgelegten möglichen Zuständen.
+            ///Dabei ist jeder Zustand an einen Integer-Wert gekoppelt, wodurch eine explizite Umwandlung (Cast) möglich ist. (vgl. Datentyp-Definition unten)
+
+            #region Anti-Beispiel -> Der Blues der C/C++ Entwickler
+            //So bitte nicht!!!!!
+            string[] wochentageStringArray = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
+
+            //So bitte nicht!!!!!
+            if (wochentageStringArray[3] == "Mi")
+            {
+                
+            }
+            #endregion
+
+            #region Ausgabe eines Enums
+            Wochentag wochentag = Wochentag.Di;
+            Console.WriteLine($"Heute is also {wochentag}"); //Di
+            #endregion
+
+
+            #region Enum und sein Index
+
+            //For-Schleife über die möglichen Zustande des Enumerators
+            Console.WriteLine("Welcher Wochentag ist dein Lieblingstag?");
+            for (int i = 1; i < 8; i++)
+            {
+                Console.WriteLine($"{i}: {(Wochentag)i}");
+            }
+            #endregion
+
+            //Speichern einer Benutzereingabe (Int) als Enumerator
+            //Cast: Int -> Wochentag
+
+            wochentag = (Wochentag)int.Parse(Console.ReadLine());
+            Console.WriteLine($"Dein Lieblingstag ist alsoi {wochentag}");
+
+            #endregion
+        }
+    }
+
+
+    enum Wochentag { Mo=1, Di, Mi, Do, Fr, Sa, So}
+}
