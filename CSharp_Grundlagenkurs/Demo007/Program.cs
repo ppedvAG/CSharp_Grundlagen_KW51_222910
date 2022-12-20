e9455e732e2f10e8b86f3627635a48ec8a4db9bc
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-12-20 14:04:19 +0100

    Modul007_Demo

---
+++ CSharp_Grundlagenkurs/Demo007/Program.cs
@@ -0,0 +1,184 @@
+﻿namespace Demo007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Methoden/Properties (Substring, StartWith, ...) VS. Statische Methoden/Properties 
+
+            //Klassen-Methoden stellen die Funktionalität eines Objektes sicher
+            string str = "Hallo Welt";
+
+            if (str.StartsWith("Hallo"))
+            {
+                string subString = str.Substring(0, 5);
+            }
+
+
+            //Statische Properties / Methoden stellen eine Hilfestellung für den Datentyp bereit
+            string leereString = string.Empty;
+
+            if (string.IsNullOrEmpty(leereString))
+            {
+
+            }
+            #endregion
+
+
+            #region Statische Methdoden
+            //Lebewesen1 (Default)
+            Lebewesen lebewesen = new Lebewesen();
+            //lebewesen.????
+            //Statische Methoden werden nicht via Instanz angeboten. Sondern hängen am Datentyp
+
+            int anzahl = Lebewesen.AnzahlLebewesen;
+            Lebewesen.ZeigeAnzahlLebewese();
+
+
+            #endregion
+
+
+            #region Statische Methoden -> Sample 2
+
+            Lebewesen lebewesen1 = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+            Lebewesen lebewesen2 = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+            Lebewesen lebewesen3 = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+
+
+
+            Console.WriteLine(Lebewesen.ZeigeAnzahlLebewese());
+            #endregion
+
+        }
+    }
+
+
+
+    public class Lebewesen
+    {
+
+
+        #region statische Methoden/Variablen
+        public static int AnzahlLebewesen { get; private set; } = 0; //Default-Wert 0 (geht auch bei anderen Properties ;-) )
+
+        public static string ZeigeAnzahlLebewese()
+        {
+            return $"Es gibt {AnzahlLebewesen} Lebewesen";
+        }
+        #endregion
+
+        #region Felder und Eigentschaten -> Kapslung von Feldern 
+        //Felder
+
+        //FELDER (Membervariablen) sind die Variablen einzelner Objekte, welche die Zustände dieser Objekte definieren
+        private string bezeichnung;
+        private double gewicht;
+
+        //Properties (Eigentschaften)
+
+
+        //EIGENSCHAFTEN (Properties) definieren mittels Getter/Setter den Lese-/Schreibzugriff für jeweils ein Feld.
+        ///In den Eigenschaften können bestimmte Bedingungen für das Lesen und Schreiben der Felder gesetzt werden, sowie der Zugriff
+        ///für Lesen und Schreiben einzeln angepasst werden
+        //Snippet: propfull + tab
+        public string Bezeichnung
+        {
+            set
+            {
+                bezeichnung = value;
+            }
+
+            get
+            {
+                return bezeichnung;
+            }
+        }
+
+
+        public double Gewicht
+        {
+            //set;get;
+            set
+            {
+                if (value > 0)
+                    gewicht = value;
+                else Console.WriteLine("Lebewesen haben kein negatives Gewicht");
+            }
+
+            get
+            {
+                return gewicht;
+            }
+        }
+
+        //Wird in einer Eigenschaft keine Spezifizierung angegeben, generiert das Programm das entsprechende Feld unsichtbar im Hintergrund
+        //Snippet: prop
+        public string Lieblingsnahrung { get; set; }
+
+        public DateTime Geburtsdatum { get; set; }
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public int Alter
+        {
+            get { return ((DateTime.Now - this.Geburtsdatum).Days / 365); }
+        }
+
+
+        #endregion
+
+        
+
+        #region Konstruktoren
+        //Wenn eine Klasse keinen Konstrukor implementiert hat, baut der Kompiler einen Default-Konstruktor dazu
+
+        //Default Konstruktor
+        public Lebewesen()
+        {
+            AnzahlLebewesen++;
+        }
+
+        //Werte-Konstruktor (Version1)
+        public Lebewesen(string bezeichnung, double gewicht, string lieblingsnahrung, DateTime geburtsdatum)
+            : this()
+        {
+            Bezeichnung = bezeichnung;
+            Gewicht = gewicht;
+            Lieblingsnahrung = lieblingsnahrung;
+            Geburtsdatum = geburtsdatum;
+        }
+
+        //Werte-Konstruktor (Version2) -> Verketten die Konstrutkoren 
+        public Lebewesen(string bezeichnung, double gewicht, string lieblingsnahrung, DateTime geburtsdatum, double heigh, double width)
+            : this(bezeichnung, gewicht, lieblingsnahrung, geburtsdatum)
+        {
+            this.Height = heigh;
+            this.Width = width;
+        }
+
+
+
+
+        #endregion
+
+        #region Methoden
+        public void Atmen()
+        {
+            Console.WriteLine("Lebewesen Atmet");
+        }
+
+        public void Kommunizieren()
+        {
+            Console.WriteLine("Eine unscheinbare Kommunikation");
+        }
+
+        public Lebewesen GebähreKind()
+        {
+            return new Lebewesen(this.Bezeichnung, 0.5, "Kleine Hühner", DateTime.Now, 0.5, 0.5);
+        }
+        #endregion 
+    }
+
+}
