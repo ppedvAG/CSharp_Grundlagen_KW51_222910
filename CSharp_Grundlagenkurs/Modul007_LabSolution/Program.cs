078abfbd36bc2dfed347e8b950aa9aa6cffc226a
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-12-21 10:25:45 +0100

    Lab007 fertig

---
+++ CSharp_Grundlagenkurs/Modul007_LabSolution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul007_LabSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
