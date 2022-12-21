6bab7fd9bd71db1da86fe8091e419bf0284d4a9a
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-12-21 10:03:22 +0100

    Demo007

--- CSharp_Grundlagenkurs/Demo007/Program.cs
+++ CSharp_Grundlagenkurs/Demo007/Program.cs
@@ -21,7 +21,7 @@
 
             if (string.IsNullOrEmpty(leereString))
             {
-
+                Console.WriteLine("String ist leer");
             }
             #endregion
 
@@ -44,21 +44,111 @@
             Lebewesen lebewesen1 = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
             Lebewesen lebewesen2 = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
             Lebewesen lebewesen3 = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+            #endregion
 
 
+
+            #region Dekonstruktor + GC
+            lebewesen3 = null;
+
+
+
+            //EineMethode();
+
+            Lebewesen lebwesen4 = EineMethode();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
 
             Console.WriteLine(Lebewesen.ZeigeAnzahlLebewese());
             #endregion
 
+
+            #region Dekonstrukor würde auch in Main aufgerufen werden. Wenige UseCase sind dafür geeignet
+
+            Lebewesen lebwesenABC;
+
+            for (int i = 0; i < 10; i++)
+            {
+                lebwesenABC = new("Cat", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            #endregion
+
+            #region Reference Types vs. Value-Types
+
+            PersonC classP = new PersonC(30, "Anna");
+
+
+
+            PersonS structP = new PersonS(30, "Hugo");
+
+
+            //Hier werden alle Properties / Fields einzelnd kopiert
+            //weiterführende Themen -> DeepCopy / Shallow Copy
+
+
+
+            //Übergabe des Klassenobjekts (Referenztyp):
+            ///Da bei der Übergabe die Referenz des Objektes an die Methode übergeben wird, wird innerhalb der Methode
+            ///das Alter des Objekts manipuliert. Im Ergebnis ist das Objekt nach der Methode ein Jahr älter geworden.
+            Altern(classP);
+
+            //Übergabe des Structobjekts (Wertetyp):
+            ///Als Wertetyp wird das Objekt bei der Übergabe an die Methode kopiert. Die Methode manipuliert nur die Kopie.
+            ///In dem Originalobjekt sind keine Veränderungen zu beobachten. Dieses Verhalten findet scih bei allen Wertetypen.
+            Altern(structP); //Struct als Wertetyp
+
+
+            //Übergabe eines Wertetypen mittels ref
+            ///Ducrh ref wird auch bei Wertetypen die Referenz übergeben, wodurch hier eine Manipulation des Originalobjekts durchgeführt wird.
+            Altern(ref structP); //Struct als Referenz
+
+            //Ausgabe der Alter
+            Console.WriteLine($"{classP.Name}: {classP.Alter}");
+            Console.WriteLine($"{structP.Name}: {structP.Alter}");
+
+            #endregion
+
+
+
+        } //Ende Main-Methode -> Alle lokalen Instanzen werden abgebaut
+
+
+        public static Lebewesen EineMethode()
+        {
+            //lokale Instanzen gelten nur innerhalb von EineMethode() 
+            Lebewesen lebewesen1 = new("Dog1", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+            Lebewesen lebewesen2 = new("Dog12", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+            Lebewesen lebewesen3 = new("Dog123", 5.0, "Lassagne", new DateTime(1999, 2, 4), 1.2, 1.2);
+
+            return lebewesen3; //Übergebe eine Reference 
+
+        } //Instanzen werden hier nicht mehr im Programm verwendet
+
+        //Methoden, welche jeweils die Alter-Property manipulieren
+        public static void Altern(PersonC person)
+        {
+            person.Alter++;
         }
+
+        public static void Altern(PersonS person) //Neuer Speicheradresse und lokale Kopie wird hochgezählt
+        {
+            person.Alter++;
+        }
+
+        public static void Altern(ref PersonS person) //Struct wird als Reference übergeben
+        {
+            person.Alter++;
+        }
+
     }
 
 
 
     public class Lebewesen
     {
-
-
         #region statische Methoden/Variablen
         public static int AnzahlLebewesen { get; private set; } = 0; //Default-Wert 0 (geht auch bei anderen Properties ;-) )
 
@@ -103,7 +193,8 @@
             {
                 if (value > 0)
                     gewicht = value;
-                else Console.WriteLine("Lebewesen haben kein negatives Gewicht");
+                else 
+                    Console.WriteLine("Lebewesen haben kein negatives Gewicht");
             }
 
             get
@@ -158,6 +249,24 @@
             this.Width = width;
         }
 
+        //Kopierkonstruktor -> Für Version 
+        public Lebewesen(Lebewesen lebewesen)
+            : this()
+        {
+            this.Gewicht = lebewesen.Gewicht;
+            this.Width = lebewesen.Width;
+            this.Height = lebewesen.Height;
+            this.Bezeichnung = lebewesen.Bezeichnung;
+            this.Geburtsdatum = lebewesen.Geburtsdatum;
+            this.Lieblingsnahrung = lebewesen.Lieblingsnahrung;
+        }
+
+        ~Lebewesen()
+        {
+            AnzahlLebewesen--;
+            Console.WriteLine($"Dekonstruktor wird aufgerufen und {Bezeichnung} wird abgebaut");
+        }
+
 
 
 
@@ -180,5 +289,35 @@
         }
         #endregion 
     }
+
+    #region Referenztyp vs. Wertetyp
+
+
+    //Klasse, deren Objekte als REFERENZTYPEN betrachtet werden
+    class PersonC
+    {
+        public int Alter { get; set; }
+        public string Name { get; set; }
+
+        public PersonC(int a, string n)
+        {
+            Alter = a;
+            Name = n;
+        }
+    }
+
+    //Struct, dessen Objekte, wie sämtliche Basisdatentypen, als WERTETYPEN betrachtet werden
+    struct PersonS
+    {
+        public int Alter { get; set; }
+        public string Name { get; set; }
+
+        public PersonS(int a, string n)
+        {
+            Alter = a;
+            Name = n;
+        }
+    }
+    #endregion
 
 }
---
+++ CSharp_Grundlagenkurs/StringBuilderBenchmarkDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace StringBuilderBenchmarkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ein string wird als feste im Arbeitsspeicher reserviert (6 Speicheraddresse) 
+            string hallo = "Hallo ";
+            hallo += "Kevin";
+
+
+
+
+            string aufbauenderString = string.Empty;
+
+            //System.Diagnostics.Stopwatch
+            
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                aufbauenderString += i.ToString(); //Performance - Killer! 
+            }
+            stopwatch.Stop();
+            long testErgebnis1 = stopwatch.ElapsedMilliseconds;
+
+
+            Console.WriteLine("Taste drücken......");
+            Console.ReadKey();
+
+
+            //System.Text;
+            StringBuilder sb = new StringBuilder();
+
+            Stopwatch stopwatch1 = new Stopwatch();
+            stopwatch1.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.Append(i); //Strings werden mit Append hinzugefügt 
+            }
+            string output = sb.ToString(); //ToString() = kompletter String wird ausgelifert 
+            stopwatch1.Stop();
+
+            long testErgebnis2 = stopwatch1.ElapsedMilliseconds;
+
+            Console.WriteLine("Benchmark Endergebnis");
+            Console.WriteLine($"Ergebnis aus einfachen addieren - Zeit: {testErgebnis1}");
+            Console.WriteLine($"Testergebnis - StringBuilder - Zeit: {testErgebnis2}");
+            Console.ReadLine();
+        }
+    }
+}
